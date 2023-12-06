e0b9841f2c882a1ce7fc71d995c4811fa328a496
Shubham-Tripathi-au45 <100478719+Shubham-Tripathi-au45@users.noreply.github.com>
2023-12-06 12:39:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 1024;
 
 app.get('/sayHello', (req, res) => {
     const jsonResponse = { message: 'Hello User' };
